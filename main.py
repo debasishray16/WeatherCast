3d1c9066280960b699a7bc36be5b15e66e824394
debasisray16 <debasishray3275@gmail.com>
2023-12-21 19:43:34 +0530

    data and api added

--- main.py
+++ main.py
@@ -22,8 +22,30 @@ def getWeather():
     obj=TimezoneFinder()
     
     result=obj.timezone_at(lng=location.longitude,lat=location.latitude)
-    print(result)
 
+    home=pytz.timezone(result)
+    local_time=datetime.now(home)
+    current_time=local_time.strftime("%I:%M %p")
+    clock.config(text=current_time)
+    name.config(text="CURRENT WEATHER")
+    
+    # weather
+    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=0093a72c975dfd0abc2d43dd629f209c"
+    json_data=requests.get(api).json()
+    condition=json_data['weather'][0]['main']
+    description=json_data['weather'][0]['descriptions']
+    temp=int(json_data['main']['temp']-273.15)
+    pressure=json_data['main']['pressure']
+    humidity=json_data['main']['humidity']
+    wind=json_data['wind']['speed']
+    
+    t.config(text=(temp, "°"))
+    c.config(text=(condition, "|", "FEELS", "LIKE", temp, "°"))
+    
+    w.config(text=wind)
+    h.config(text=humidity)
+    d.config(text=description)
+    p.config(text=pressure)
 
 
 # search box
@@ -41,7 +63,7 @@ textfield.focus()
 
 search_icon=PhotoImage(file="images/search_icon.png")
 myimage_icon=Button(image=search_icon,borderwidth=0,cursor="hand2",
-                    bg="#404040",command=getWeather)
+                bg="#404040",command=getWeather)
 myimage_icon.place(x=420,y=34)
 
 
@@ -57,6 +79,11 @@ frame_image=Label(image=Frame_image)
 frame_image.pack(padx=5,pady=5,side=BOTTOM)
 
 
+# time
+name=Label(root,font=("arial",15,"bold"))
+name.place(x=30,y=100)
+clock=Label(root,font=("Hevlvetica",20))
+clock.place(x=30,y=140)
 
 # Label
 label1=Label(root,text="WIND",font=("Ubuntu",13,"bold"),fg="white",bg="#1ab5ef")
@@ -76,17 +103,22 @@ t.place(x=400,y=150)
 c=Label(font=("arial",15,"bold"))
 c.place(x=400,y=250)
 
+# Wind data
 w=Label(text="...",font=("Ubuntu",16,"bold"),bg="#1ab5ef")
 w.place(x=130,y=430)
 
+# Humidity data
 h=Label(text="...",font=("Ubuntu",16,"bold"),bg="#1ab5ef")
 h.place(x=265,y=430)
 
-f=Label(text="...",font=("Ubuntu",16,"bold"),bg="#1ab5ef")
-f.place(x=450,y=430)
+# 
+d=Label(text="...",font=("Ubuntu",16,"bold"),bg="#1ab5ef")
+d.place(x=450,y=430)
 
-l = Label(text="...", font=("Ubuntu", 16, "bold"), bg="#1ab5ef")
-l.place(x=670, y=430)
+
+# Pressure data
+p = Label(text="...", font=("Ubuntu", 16, "bold"), bg="#1ab5ef")
+p.place(x=670, y=430)
 
 
 
